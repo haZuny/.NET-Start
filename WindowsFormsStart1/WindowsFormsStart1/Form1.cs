ea9a479fcc53e87d18f2bc35fc1e6497fec4377a
haZuny <gkwns5791@naver.com>
2024-11-14 17:11:13 +0900

    윈폼 스타트

---
+++ WindowsFormsStart1/WindowsFormsStart1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsStart1
+{
+    public partial class Back : Form
+    {
+        public Back()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = textBox1.Text;
+            MessageBox.Show("버튼을 클릭했습니다");
+        }
+    }
+}
